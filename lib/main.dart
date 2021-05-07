1e761e109b317b072f1e990aa8497dd154c1af35
mukundkr14 <mukundkr14@gmail.com>
2021-05-08 03:09:30 +0530

    ch3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,67 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "MyApp",
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      title: "MyApp",
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.indigo),
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("MyApp"),
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            alignment: Alignment.center,
-            // margin: const EdgeInsets.all(8),
-            // color: Colors.teal,
-            // color not supported with decoration, use color inside decoration
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-                // shape: BoxShape.circle,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey[900],
-                      blurRadius: 5.0,
-                      offset: Offset(4.0, 4.0))
-                ],
-                color: Colors.teal,
-                gradient: LinearGradient(colors: [Colors.blue, Colors.white])),
-            child: Text(
-              "I am a box",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
+      appBar: AppBar(
+        title: Text("MyApp"),
+      ),
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Mukund Kumar"),
+              accountEmail: Text("mukundkr14@gmail.com"),
+              //   currentAccountPicture: Image.network(
+              //       "https://images.unsplash.com/photo-1455274111113-575d080ce8cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"),
+              // ),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1455274111113-575d080ce8cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"),
+              ),
             ),
-          ),
-        ));
+            // DrawerHeader(
+            //   child: Text(
+            //     "Hi I am drawer",
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.indigo),
+
+            // ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Email"),
+              subtitle: Text("mukundkr14@gmail.com"),
+              trailing: Icon(Icons.send),
+            )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+        mini: true,
+      ),
+    );
   }
 }
