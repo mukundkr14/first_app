06cef449538bd4e428716ca4c13293f7383425fd
mukundkr14 <mukundkr14@gmail.com>
2021-05-08 23:21:50 +0530

    ch4 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../change_name_card.dart';
 import '../drawer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,11 +12,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
   var myText = "Change Me";
+  // var url = Uri.parse("https://jsonplaceholder.typicode.com/photos");
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+
+    getData() async {
+      // http.Response res = await http.get(url);
+      var res = await http.get(Uri.parse(url));
+      // print(res.body);
+      data = jsonDecode(res.body);
+      print(data);
+      setState(() {});
+    }
+
+    getData();
   }
 
   @override
@@ -27,12 +41,32 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child:
-                ChangeNameCard(myText: myText, nameController: _nameController),
-          ),
-        ),
+        child: data != null
+            // ? GridView.builder(
+            //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //         crossAxisCount: 2),
+            //     itemBuilder: (context, index) {
+            //       return ListTile(
+            //         title: Text(data[index]["title"]),
+            //         subtitle: Text(
+            //             "ID: ${data[index]["id"]}"), //string interpolation used here to convert int to string
+            //         leading: Image.network(data[index]["url"]),
+            //       );
+            //     },
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(data[index]["title"]),
+                    subtitle: Text(
+                        "ID: ${data[index]["id"]}"), //string interpolation used here to convert int to string
+                    leading: Image.network(data[index]["url"]),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
